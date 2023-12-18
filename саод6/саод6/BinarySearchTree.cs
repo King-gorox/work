9090d69d5fe1084da7f33fc0aed48c3cac85da85
King-gorox <144322973+King-gorox@users.noreply.github.com>
2023-12-18 19:58:33 +0300

    saod 6

---
+++ саод6/саод6/BinarySearchTree.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Binary_Search_Tree_lesson_6
+{
+    internal class BinarySearchTree<T>
+    {
+        private Node<T> root;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public void Insert(T key)
+        {
+            root = InsertRec(root, key);
+        }
+        
+        private bool SearchRec(Node<T> root, T key)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (Comparer<T>.Default.Compare(key, root.Key) == 0)
+            {
+                return true;
+            }
+
+            if (Comparer<T>.Default.Compare(key, root.Key) < 0)
+            {
+                return SearchRec(root.Left, key);
+            }
+             return SearchRec(root.Right, key);
+        }
+
+        private Node<T> InsertRec(Node<T> root, T key)
+        {
+            if (root == null)
+            {
+                root = new Node<T>(key);
+                return root;
+            }
+
+            if (Comparer<T>.Default.Compare(key, root.Key) < 0)
+            {
+                root.Left = InsertRec(root.Left, key);
+            }
+            else if (Comparer<T>.Default.Compare(key, root.Key) > 0)
+            {
+                root.Right = InsertRec(root.Right, key);
+            }
+
+            return root;
+        }
+
+        public void InOrderTraversal()
+        {
+            InOrderTraversal(root);
+        }
+
+        private void InOrderTraversal(Node<T> root, string indent = "", bool isRight = false)
+        {
+            if (root != null)
+            {
+                Console.WriteLine(indent + (isRight ? "R-" : "L-") + root.Key);
+                InOrderTraversal(root.Left, indent + (isRight ? "|  " : "    "));
+                InOrderTraversal(root.Right, indent + (isRight ? "|  " : "    "), true);
+            }
+        }
+
+        public bool Search(T key)
+        {
+            return SearchRec(root, key);
+        }
+        public int LeafCount()
+        {
+            return CountLeaves(root);
+        }
+
+        private int CountLeaves(Node<T> node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            if (node.Left == null && node.Right == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return CountLeaves(node.Left) + CountLeaves(node.Right);
+            }
+        }
+    }
+}
---
+++ саод6/саод6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Data;
+
+namespace Binary_Search_Tree_lesson_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            BinarySearchTree<int> tree = new BinarySearchTree<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                int valuie = (int.Parse(i.ToString() + i.GetHashCode().ToString()) 
+                    * currentTime.Ticks.GetHashCode() / 1466) % 100 ;
+                tree.Insert(valuie);
+            }
+            tree.Insert(5);
+            tree.InOrderTraversal();
+
+            int searchValue = 5;
+            bool found = tree.Search(searchValue);
+
+            if (found)
+            {
+                Console.WriteLine($"\n Найдено значение {searchValue}");
+            }
+            else
+            {
+                Console.WriteLine($"\n Не найденозначение {searchValue}");
+            }
+            int leafCount = tree.LeafCount();
+            Console.WriteLine($"\n Количество листьев в дереве: {leafCount}");
+        }
+    }
+}
