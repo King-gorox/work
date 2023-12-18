faddfef3352450d8d1fa2efb1304ccf34b1ab8e5
King-gorox <144322973+King-gorox@users.noreply.github.com>
2023-12-18 19:55:25 +0300

    saod 3

---
+++ саод3/саод3/list.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace саод3
+{
+    public class CustomList<T> : IEnumerable<T>
+    {
+        private T[] items;
+        private int capacity;
+        private int count;
+
+        public CustomList()
+        {
+            capacity = 4;
+            items = new T[capacity];
+            count = 0;
+        }
+
+        public int Count => count;
+
+        public void Add(T item)
+        {
+            if (count == capacity)
+            {
+                capacity *= 2;
+                Array.Resize(ref items, capacity);
+            }
+
+            items[count] = item;
+            count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            Array.Copy(items, index + 1, items, index, count - index - 1);
+            count--;
+        }
+
+        public T this[int index]
+        {
+            get => (index < 0 || index >= count) ? throw new Exception(nameof(index)) : items[index];
+            set
+            {
+                if (index < 0 || index >= count)
+                    throw new Exception(nameof(index));
+
+                items[index] = value;
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+                yield return items[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
+
